1aa6899ef6879f9777172fa0a38059174e881887
IgorLapuzo <104383591+IgorLapuzo@users.noreply.github.com>
2022-09-28 21:33:04 +0300

    connected DB, described models

--- server/index.js
+++ server/index.js
@@ -1,8 +1,27 @@
 require('dotenv').config()
 const express = require('express')
-
+const sequelize = require('./db')
+const models = require('./models/models.js')
 const PORT = process.env.PORT || 5000
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
+app.use(express.json())
 
-app.listen(PORT, () => console.log(`Server has started on port ${PORT}`))
+app.get('/', (req, res) => {
+	res.status(200).json({message: 'WORKING'})
+})
+
+
+const start = async () => {
+	try{
+		await sequelize.authenticate()
+		await sequelize.sync()
+		app.listen(PORT, () => console.log(`Server has started on port ${PORT}`))
+	} catch (e) {
+		console.log(e)
+	}
+}
+
+start()
