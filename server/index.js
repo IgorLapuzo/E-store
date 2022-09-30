36e771c23a9b063ed7721232555fb149c3c507e2
IgorLapuzo <104383591+IgorLapuzo@users.noreply.github.com>
2022-09-30 21:06:38 +0300

    added error handling

--- server/index.js
+++ server/index.js
@@ -5,11 +5,13 @@ const models = require('./models/models.js')
 const PORT = process.env.PORT || 5000
 const cors = require('cors')
 const router = require('./routes/index')
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/api', router)
+app.use(errorHandler)
 
 
 const start = async () => {
