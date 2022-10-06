ea937f33c74fb54b075499aa0ccfe587a5ab3880
IgorLapuzo <104383591+IgorLapuzo@users.noreply.github.com>
2022-10-06 22:32:06 +0300

    added user store, device store

---
+++ client/src/store/DeviceStore.js
@@ -0,0 +1,39 @@
+import { makeAutoObservable } from 'mobx';
+
+export default class DeviceStore {
+	constructor() {
+		this._types = [
+			{id: 1, name: 'Холодильники'},
+			{id: 1, name: 'Смартфоны'},
+		]
+		this._brands = [
+			{id: 1, name: 'Samsung'},
+			{id: 1, name: 'Apple'},
+		]
+		this._devices = [
+			{id: 1, name: 'Iphone 12 pro', price: 1100, rating: 5, img: 'aaa'},
+			{id: 2, name: 'Iphone 13 pro', price: 2100, rating: 0, img: 'bbb'}
+		]
+		makeAutoObservable(this)
+	}
+
+	setTypes(types) {
+		this._types = types
+	}
+	setBrands(brands) {
+		this._brands = brands
+	}
+	setDevices(devices) {
+		this._devices = devices
+	}
+
+	get types() {
+		return this._types
+	}
+	get brands() {
+		return this._brands
+	}
+	get devices() {
+		return this._devices
+	}
+}
