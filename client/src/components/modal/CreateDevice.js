19d4c31de0dcba49d8c3eec25e9fe1395300151f
IgorLapuzo <104383591+IgorLapuzo@users.noreply.github.com>
2022-10-16 22:50:16 +0300

    finished admin panel

--- client/src/components/modal/CreateDevice.js
+++ client/src/components/modal/CreateDevice.js
@@ -1,13 +1,23 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import { Context } from '../../index';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Dropdown from 'react-bootstrap/Dropdown';
-import { Context } from '../../index';
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 
 
 const CreateDevice = ({ show, onHide }) => {
 	const {device} = useContext(Context)
+	const [info, setInfo] = useState([])
+
+	const addInfo = () => {
+		setInfo([...info, {title: '', description: '', number: Date.now()}])
+	}
+	const removeInfo = (number) => {
+		setInfo(info.filter(i => i.number !== number))
+	}
 
 	return (
 		<Modal
@@ -22,14 +32,64 @@ const CreateDevice = ({ show, onHide }) => {
 			</Modal.Header>
 			<Modal.Body>
 				<Form>
-					<Dropdown>
+					<Dropdown className='mt-2 mb-2'>
 						<Dropdown.Toggle>Chose device</Dropdown.Toggle>
 						<Dropdown.Menu>
 							{device.types.map(type =>
 								<Dropdown.Item key={type.id}>{type.name}</Dropdown.Item>
 							)}
 						</Dropdown.Menu>
-					</Dropdown>					
+					</Dropdown>	
+					<Dropdown className='mt-2 mb-2'>
+						<Dropdown.Toggle>Chose brand</Dropdown.Toggle>
+						<Dropdown.Menu>
+							{device.brands.map(brand =>
+								<Dropdown.Item key={brand.id}>{brand.name}</Dropdown.Item>
+							)}
+						</Dropdown.Menu>
+					</Dropdown>		
+					<Form.Control
+						className='mt-3'
+						placeholder={'Enter device\'s name'}
+					/>	
+					<Form.Control
+						className='mt-3'
+						placeholder={'Enter device\'s price'}
+						type='number'
+					/>	
+					<Form.Control
+						className='mt-3'
+						type='file'
+					/>	
+					<hr/>
+					<Button
+						variant={'outline-dark'}
+						onClick={addInfo}
+					>
+						Add new property
+					</Button>
+					{info.map(i =>
+						<Row className='mt-4' key={i.number}>
+							<Col md={4}>
+								<Form.Control
+									placeholder={'Enter property\'s name'}
+								/>
+							</Col>
+							<Col md={4}>
+								<Form.Control
+									placeholder={'Enter property\'s description'}
+								/>
+							</Col>
+							<Col md={4}>
+								<Button 
+									onClick={() => removeInfo(i.number)}
+									variant={'outline-danger'}
+								>
+									Delete
+								</Button>
+							</Col>
+						</Row>	
+					)}
 				</Form>
 			</Modal.Body>
 			<Modal.Footer>
