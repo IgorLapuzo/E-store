82fb8b84be558a8e2e4f27672b40a79326efd229
IgorLapuzo <104383591+IgorLapuzo@users.noreply.github.com>
2022-10-28 23:09:49 +0300

    added pagination

--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -7,6 +7,9 @@ export default class DeviceStore {
 		this._devices = []
 		this._selectedType = {}
 		this._selectedBrand = {}
+		this._page = 1
+		this._totalCount = 8
+		this._limit = 3
 		makeAutoObservable(this)
 	}
 
@@ -20,12 +23,20 @@ export default class DeviceStore {
 		this._devices = devices
 	}
 	setSelectedType(type) {
+		this.setPage(1)
 		this._selectedType = type
 	}
 	setSelectedBrand(brand) {
+		this.setPage(1)
 		this._selectedBrand = brand
 	}
-
+	setPage(page) {
+		this._page = page
+	}
+	setTotalCount(count) {
+		this._totalCount = count
+	}
+	
 	get types() {
 		return this._types
 	}
@@ -40,5 +51,14 @@ export default class DeviceStore {
 	}
 	get selectedBrand() {
 		return this._selectedBrand
+	}
+	get totalCount() {
+		return this._totalCount
+	}
+	get page() {
+		return this._page
+	}
+	get limit() {
+		return this._limit
 	}
 }
